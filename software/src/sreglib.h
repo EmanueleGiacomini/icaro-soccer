349c8faa93f6568fb9e777b222888b197e4b45ba
Your Name <monefede@libero.it>
2018-02-07 18:42:49 +0100

    Libreria modificata

--- software/src/sreglib.h
+++ software/src/sreglib.h
@@ -11,4 +11,19 @@
 #define sreglib_h
 #include <Arduino.h>
 
+class ShiftRegister{
+  int _pin_latch;
+  int _pin_clock;
+  int _pin_data;
+  int _data_byte;
+  public:
+
+  //costruttore
+  ShiftRegister();
+
+  //metodi
+  void inizializza(int pin_latch,int pin_clock,int pin_data);
+  void impostaBit(int bit, int valore);
+  void elabora();
+};
 #endif
